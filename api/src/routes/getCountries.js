211feebf082377b4ea5d18edca79d5b900a073c2
FedexaZ <fedexaz3@gmail.com>
2022-02-12 15:51:50 -0300

    Agregando el Post activity

--- api/src/routes/getCountries.js
+++ api/src/routes/getCountries.js
@@ -1,11 +1,88 @@
 const { Router } = require('express');
+const { Country, Activity } = require('../db');
+const { Op } = require("sequelize");
+const axios = require('axios');
 
-const route = Router()
+const route = Router();
 
-route.get('/:idPais', (req, res) => {
-    const { idPais } = req.params;
-    const { name } = req.query;
-    
+const loadDB = async () => {
+    const existe = await Country.findByPk('ARG');
+
+    if(!existe){
+        const respuesta = await axios.get('https://restcountries.com/v3/all')
+
+        try {
+            respuesta.data.forEach(async (e) => {
+                let cap = "None";
+
+                if(Array.isArray(e.capital)){
+                    let cap = e.capital.pop();
+                }
+
+                await Country.create({
+                    ID: e.cca3,
+                    name: e.name.common,
+                    urlImg: e.flags[1],
+                    continent: e.region,
+                    capital: cap,
+                    subregion: e.subregion,
+                    area: e.area,
+                    poblacion: e.population
+                })
+            })
+            console.log("Datos traidos de la API y cargados en la DB");
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    else{
+        console.log("Base de datos cargada!")
+    }
+}
+
+
+route.get('/', async (req, res) => {
+    await loadDB();
+
+    const { name } = req.query;  
+
+    if(name){
+        const resultado = await Country.findAll({
+            where: {
+                name:{
+                    [Op.iLike]: `%${name}%`
+                }
+            }
+        })
+
+        if(resultado.length === 0){
+            return res.status(404).json({
+                msg: "País no encontrado"
+            })
+        }
+
+        res.json(resultado)
+    }
+    else{
+        const resultado = await Country.findAll()
+        res.json(resultado)
+    }
+
+})
+
+route.get('/:idPais', async (req, res) => {
+    const { idPais } = req.params
+
+    const existe = await Country.findAll({
+        where:{
+            ID: idPais
+        },
+        include: Activity
+    })
+
+    if(existe){
+        res.json(existe)
+    }
 })
 
 module.exports = route;
--- api/src/routes/postActivity.js
+++ api/src/routes/postActivity.js
@@ -1,9 +1,36 @@
 const { Router } = require('express');
+const { Country, Activity } = require("../db");
 
 const route = Router()
 
-route.post('/', (req, res) => {
-    res.send("ACTIVIDADES OK")
+const crearActividad = async (name, dificultad, duracion, temporada, idPais) => {
+    const act = await Activity.create({
+        name,
+        dificultad,
+        duracion,
+        temporada
+    })
+
+    await act.addCountry(idPais)
+    console.log("Actividad agregada")
+}
+
+route.post('/', async (req, res) => {
+    const { name, dificultad, duracion, temporada, idPais } = req.body;
+
+    crearActividad(name, dificultad, duracion, temporada, idPais)
+
+    /*if(Array.isArray(idPais)){
+
+    }else{
+
+    } */
+
+    const result = await Activity.findAll({
+        include: Country
+    })
+    console.log("-------------------------> \n",result);
+    res.send(result)
 })
 
 module.exports = route;
