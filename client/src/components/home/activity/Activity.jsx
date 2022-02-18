b56880dd740ea0465ebb05a2c55dec4e6d2e529e
FedexaZ <fedexaz3@gmail.com>
2022-02-18 01:27:32 -0300

    Programando control del formulario de addactivity

--- client/src/components/home/activity/Activity.jsx
+++ client/src/components/home/activity/Activity.jsx
@@ -6,60 +6,67 @@ import style from './css/activity.module.css'
 
 export default function Activity() {
     const [input, setInput] = useState({
-        name: ' ',
+        name: '',
         dificultad: 0,
         duracion: 0,
         temporada: 'verano',
         pais: [],
-        errors: {
-            dificultad: '',
-            duracion: '',
-            temporada: '',
-            pais: '',
-        },
         disabled: true
+    })
+
+    const [error, setError] = useState({
+        name: '',
+        dificultad: '',
+        duracion: '',
+        temporada: '',
+        pais: '',
     })
 
     function handleChange(e){
         const { name, value } = e.target;
-        let errors = this.state.errors;
-        
-        switch (name) {
-            case 'name': 
-                errors.name = value.length < 5 ? 'Nombre debe tener 5 caracteres o más!' : '';
-            break;
-            case 'duracion': 
-                errors.duracion = value < 1 && 1001 > value ? 'La duración debe ser mayor a 1 y menor a 1000' : '';
-            break;
-            case 'dificultad': 
-                errors.dificultad = value < 0 && 101 > value ? 'La dificultad debe ser mayor o igual a 0 y menor a 100' : '';
-            break;
-            default:
-            break;
+
+        switch(name){
+            case 'name':{
+                setError({
+                    ...error,
+                    name: value.length < 3 ? 'El nombre es demasiado corto (3 o mas letras)' : '' 
+                })
+                break;
+            }
+
+            case 'dificultad':{
+                setError({
+                    ...error,
+                    dificultad: value < 1 || value > 5 ? 'La dificultad varía entre 1 y 5' : '' 
+                })
+                break;
+            }
+            
+            case 'duracion':{
+                setError({
+                    ...error,
+                    duracion: value < 1 || value > 2000 ? 'La duración no puede ser mas de 2000 horas ni menor a 0 horas' : '' 
+                })
+                break;
+            }
+            
+            case 'temporada':{
+                setError({
+                    ...error,
+                    temporada: value === 'seleccionar' ? 'Elige una temporada' : '' 
+                })
+                break;
+            }
+
+            default:{
+                break;
+            }
         }
+        
         setInput({
             ...input,
-            [name]: value,
-            errors
+            [name]: value
         })
-        validarForm(input.errors)
-        
-    }
-
-    function validarForm(errors) {
-        let valid = true;
-        Object.values(errors).forEach((val) => val.length > 0 && (valid = false));
-        if(valid) {
-            setInput({
-                ...input,
-                disabled: false
-            })
-        } else {
-            setInput({
-                ...input,
-                disabled: true
-            })
-        }
     }
 
     function handleSubmit(e){
@@ -71,19 +78,29 @@ export default function Activity() {
         <NavBar2 />
         <form className={style.container} method="POST" onSubmit={handleSubmit}>
             <h1>Agregar actividad Turística</h1>
+
             <label htmlFor="name">Nombre de la actividad:</label>
             <input type="text" name="name" id="name" placeholder='Ingresa un nombre' value={input.name} onChange={handleChange} />
+            {error.name.length ? <span>{error.name}</span> : null }
+
             <label htmlFor="dificultad">Dificultad de la actividad:</label>
             <input type="number" min='0' name="dificultad" id="dificultad" placeholder='Ingresa una dificultad' value={input.dificultad} onChange={handleChange} />
+            {error.dificultad.length ? <span>{error.dificultad}</span> : null }
+
             <label htmlFor="duracion">Duración de la actividad:</label>
-            <input type="number" min='0' name="duracion" id="duracion" placeholder='Ingresa una duración' value={input.duracion} onChange={handleChange} />
+            <input type="number" min='0' max='2001' name="duracion" id="duracion" placeholder='Ingresa una duración' value={input.duracion} onChange={handleChange} />
+            {error.duracion.length ? <span>{error.duracion}</span> : null }
+
             <label htmlFor='temporada'>Temporada de la actividad:</label>
             <select name="temporada" id="temporada" value={input.temporada} onChange={handleChange}>
+                <option value="seleccionar" selected>Seleccionar...</option>
                 <option value="invierno">Invierno</option>
                 <option value="otonio">Otoño</option>
                 <option value="primavera">Primavera</option>
                 <option value="verano">Verano</option>
             </select>
+            {error.temporada.length ? <span>{error.temporada}</span> : null }
+
             <label htmlFor="paisAct">¿En que país está la actividad?</label>
             <input type="text" name="bpais" placeholder='Escribe el nombre de algún país a agregar...'/>
             {true ?
