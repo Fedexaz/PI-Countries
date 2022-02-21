11bda9eb7ce88f5dc76e8cd95378ac18931a6810
FedexaZ <fedexaz3@gmail.com>
2022-02-20 23:10:31 -0300

    cambios en index.js

--- api/index.js
+++ api/index.js
@@ -21,8 +21,8 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once
-conn.sync({ force: true }).then(() => { 
-  server.listen(process.env.PORT || 5000, () => {
+conn.sync({ force: false }).then(() => { 
+  server.listen(process.env.PORT, () => {
     console.log('listening at ' + process.env.PORT); // eslint-disable-line no-console
   });
 });
